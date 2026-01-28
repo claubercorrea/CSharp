2a804c0d6c9d54db292b096c034bd7ae64c52f85
CLAUBER CORREA <162238096+claubercorrea@users.noreply.github.com>
2026-01-27 21:34:01 -0300

    EXERCCICIOS DE CSHARP

---
+++ saida_dados_print/exercicios/exercicios/Program.cs
@@ -0,0 +1,30 @@
+﻿using exercicios;
+using System;
+namespace Execercicos
+{
+    public class  Calculos
+    {
+        public  static void Main(string[] argns)
+        {
+
+            //Calc1 Ex1 = new Calc1();
+            //Ex1.Calculando1();
+
+            //Exercicio2 Ex2 = new Exercicio2();
+            //Ex2.Calc2();
+            //exercicio3 ex3 = new exercicio3();
+            //ex3.calc3();
+
+            //Exercicio4 ex4 = new Exercicio4();
+            //ex4.Calc4();
+
+            //Exercicio5 Ex5 = new Exercicio5();
+            //Ex5.Calc5();    
+
+            Exercicio6 Ex6 = new Exercicio6();
+            Ex6.Calc6();
+        }
+
+    }
+   
+}
